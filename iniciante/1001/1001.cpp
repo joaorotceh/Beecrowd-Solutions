5c1b65e89849431f8849f074601b0bdd460b1b87
João <joaorotceh@protonmail.com>
2025-07-24 21:22:15 -0300

    Create 1001.cpp

---
+++ iniciante/1001/1001.cpp
@@ -0,0 +1,16 @@
+// 1001 - Extremamente Básico - beecrowd
+// Iniciante - Nível 4
+
+#include <iostream>
+
+int main() {
+
+    int A, B;
+    std::cin >> A >> B;
+
+    int X = A + B;
+
+    std::cout << "X = " << X << '\n';
+
+    return 0;
+}

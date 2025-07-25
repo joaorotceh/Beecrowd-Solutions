758faa1961d53de2d09c8579bd16af6c7eb80f78
João <joaorotceh@protonmail.com>
2025-07-24 21:36:25 -0300

    Some solutions
    - Created 1000.c
    - Created 1000.py

---
+++ iniciante/1001/1001.c
@@ -0,0 +1,16 @@
+// 1001 - Extremamente Básico - beecrowd
+// Iniciante - Nível 4
+
+#include <stdio.h>
+
+int main() {
+
+    int A, B;
+    scanf("%d %d", &A, &B);
+
+    int X = A + B;
+
+    printf("X = %d\n", X);
+
+    return 0;
+}
---
+++ iniciante/1001/1001.py
@@ -0,0 +1,9 @@
+# 1001 - Extremamente Básico - beecrowd
+# Iniciante - Nível 4
+
+A = int(input())
+B = int(input())
+
+X = A + B
+
+print(f"X = {X}")

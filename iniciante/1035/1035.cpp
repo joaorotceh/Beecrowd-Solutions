ac3f3111ea5b0122f74d9d9e6af985293cc89429
João <joaorotceh@protonmail.com>
2025-07-25 23:16:17 -0300

    Some solutions
    - Created 1035.c
    - Created 1035.cpp
    - Created 1035.py

---
+++ iniciante/1035/1035.cpp
@@ -0,0 +1,18 @@
+// 1035 - Teste de Seleção 1 - beecrowd
+// Iniciante - Nível 2
+
+#include <iostream>
+
+int main() {
+
+    int A, B, C, D;
+    std::cin >> A >> B >> C >> D;
+
+    if(B > C && D > A && (C + D) > (A + B) && (C > 0 && D > 0) && A % 2 == 0) {
+        std::cout << "Valores aceitos\n";
+    } else {
+        std::cout << "Valores nao aceitos\n";
+    }
+
+    return 0;
+}

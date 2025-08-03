9e78f44cda692ac633c35b42774dc62664068c7f
João <joaorotceh@protonmail.com>
2025-08-03 09:33:59 -0300

    Small fixes

--- iniciante/1041/1041.c
+++ iniciante/1041/1041.c
@@ -9,7 +9,7 @@ int main()
     scanf("%lf %lf", &x, &y);
 
     if (x == 0 && y == 0) puts("Origem");
-    else if (x == 0.0|| y == 0.0) printf("%s\n", (x == 0.0) ? "Eixo Y" : "Eixo X");
+    else if (x == 0 || y == 0) printf("%s\n", (x == 0.0) ? "Eixo Y" : "Eixo X");
     else puts(x > 0 ? (y > 0 ? "Q1" : "Q4") : (y > 0 ? "Q2" : "Q3"));
 
     return 0;
